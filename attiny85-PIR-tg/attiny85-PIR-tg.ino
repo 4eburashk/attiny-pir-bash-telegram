17e114314125767bc225fd71740384d5487cd91b
4eburashk <39623246+4eburashk@users.noreply.github.com>
2020-11-29 23:16:19 +0300

    add PIR-digispark connection scheme

--- attiny85-PIR-tg/attiny85-PIR-tg.ino
+++ attiny85-PIR-tg/attiny85-PIR-tg.ino
@@ -1,4 +1,10 @@
-/ прерывание на нажатие кнопки. смена состояния флага
+// прерывание на нажатие кнопки. смена состояния флага
+// датчик PIR припаян к платке digispark следующим образом:
+// PIR          digispark
+// GND    ---   GND
+// SIG    ---   P2
+// VCC    ---   5v
+
 #include "DigiKeyboard.h"
 volatile bool FLAG = 0; // state up/down.
 volatile uint32_t milsLast;
@@ -7,13 +13,13 @@ void setup() {
   digitalWrite(1,0);  // выкл.
   pinMode(2, INPUT_PULLUP);
   DigiKeyboard.sendKeyStroke(0);
-  // поезали командовать!
+  // поехали командовать!
   DigiKeyboard.delay(3000);// при втыкании - подождать.
   DigiKeyboard.sendKeyStroke(KEY_F3, MOD_ALT_RIGHT | MOD_CONTROL_LEFT);
   DigiKeyboard.delay(3000);
-  DigiKeyboard.println("login");
+  DigiKeyboard.println("login"); // логин пользователя от имени которого слать сообщения
   DigiKeyboard.delay(500);
-  DigiKeyboard.println("password");
+  DigiKeyboard.println("password"); // пароль тоже заменить.
   DigiKeyboard.delay(4000);
   
   attachInterrupt(0, btnInterr, FALLING); // прерывание P2 INT0
@@ -24,7 +30,7 @@ void loop() {
   if (FLAG) {
     // обработка события.
     digitalWrite(1,1);  // раз уж они есть, будем моргать.
-    DigiKeyboard.println("tg4all -pir");  // команда запуска скрипта.
+    DigiKeyboard.println("tg4all -pir");  // команда запуска скрипта. Если не в /bin, то указать путь до tg4all
     DigiKeyboard.delay(100);
     FLAG = !FLAG;
     digitalWrite(1,0);
